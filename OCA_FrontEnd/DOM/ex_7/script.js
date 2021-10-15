291a5397aa413277b7182c05b9589a20c5e51380
ahmadaldabouqii <ahmadaldabouqi97@gmail.com>
2021-10-15 15:51:50 +0300

    ex_7 done

---
+++ OCA_FrontEnd/DOM/ex_7/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const form = document.querySelector('#form');
+const songTextField = document.querySelector('#songTextSearch');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (songTextField.value) {
+    const div = document.createElement('div');
+    div.classList.add('song');
+    div.innerHTML = `${songTextField.value} <button class="remove">remove!</button>`;
+    div.lastChild.addEventListener('click', () => div.remove());
+    document.body.append(div);
+    songTextField.value = '';
+  } else {
+    alert('Add song name before Submit!!');
+  }
+});
