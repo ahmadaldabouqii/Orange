58d217f795eb891fac5a57f51d2d7de21d5a2522
ahmadaldabouqii <ahmadaldabouqi97@gmail.com>
2021-10-16 11:29:29 +0300

    solve ex

--- OCA_FrontEnd/DOM/ex_5/script.js
+++ OCA_FrontEnd/DOM/ex_5/script.js
@@ -6,44 +6,71 @@ const repeatPassword = document.querySelector('#repeatPass');
 const error_field_2 = document.querySelector('.error_field_2');
 const submitBtn = document.querySelector('#submit');
 
-password.addEventListener('keyup', () => {
-  if (
-    password.value.length < 6 &&
-    password.value !== repeatPassword.value &&
-    password.value.length !== repeatPassword.value.length
-  ) {
-    showError(error_field_1);
-  } else {
-    error_field_1.style.display = 'none';
-  }
+// password.addEventListener('keyup', () => {
+//   if (
+//     password.value.length < 6 &&
+//     password.value !== repeatPassword.value &&
+//     password.value.length !== repeatPassword.value.length
+//   ) {
+//     showError(error_field_1);
+//   } else {
+//     error_field_1.style.display = 'none';
+//   }
 
-  if (
-    password.value.length === repeatPassword.value.length &&
-    password.value === repeatPassword.value &&
-    password.value.length !== 0 &&
-    repeatPassword.value.length !== 0
-  )
-    submitBtn.style.display = 'block';
-});
+//   if (
+//     password.value.length === repeatPassword.value.length &&
+//     password.value === repeatPassword.value &&
+//     password.value.length !== 0 &&
+//     repeatPassword.value.length !== 0
+//   )
+//     submitBtn.style.display = 'block';
+// });
 
-repeatPassword.addEventListener('keyup', () => {
-  if (
-    repeatPassword.value.length > 0 &&
-    repeatPassword.value !== password.value &&
-    repeatPassword.value.length !== password.value.length
-  ) {
-    showError(error_field_2);
-  } else {
-    error_field_2.style.display = 'none';
-  }
+// repeatPassword.addEventListener('keyup', () => {
+//   if (
+//     repeatPassword.value.length > 0 &&
+//     repeatPassword.value !== password.value &&
+//     repeatPassword.value.length !== password.value.length
+//   ) {
+//     showError(error_field_2);
+//   } else {
+//     error_field_2.style.display = 'none';
+//   }
 
-  if (
-    repeatPassword.value.length === password.value.length &&
-    repeatPassword.value === password.value &&
-    repeatPassword.value.length !== 0 &&
-    password.value.length !== 0
-  )
-    submitBtn.style.display = 'block';
+//   if (
+//     repeatPassword.value.length === password.value.length &&
+//     repeatPassword.value === password.value &&
+//     repeatPassword.value.length !== 0 &&
+//     password.value.length !== 0
+//   )
+//     submitBtn.style.display = 'block';
+// });
+
+// smart solution
+const inputs = document.querySelectorAll('form input[type=password]');
+
+inputs.forEach((input, i, arr) => {
+  arr[i].addEventListener('keyup', () => {
+    let e = arr[i] === password ? error_field_1 : error_field_2;
+    if (
+      arr[i].value.length < 6 &&
+      password.value !== repeatPassword.value &&
+      password.value.length !== repeatPassword.value.length
+    ) {
+      showError(e);
+    } else {
+      e.style.display = 'none';
+    }
+
+    if (
+      password.value.length === repeatPassword.value.length &&
+      password.value === repeatPassword.value &&
+      password.value.length !== 0 &&
+      arr[i].value.length > 5 &&
+      repeatPassword.value.length !== 0
+    )
+      submitBtn.style.display = 'block';
+  });
 });
 
 function showError(err) {
