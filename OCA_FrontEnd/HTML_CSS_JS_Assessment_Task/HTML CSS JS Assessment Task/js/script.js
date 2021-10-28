5c9eb5718a9fedb17b837f307f1061a4fd7154da
ahmadaldabouqii <ahmadaldabouqi97@gmail.com>
2021-10-28 15:14:03 +0300

    cup cake project

---
+++ OCA_FrontEnd/HTML_CSS_JS_Assessment_Task/HTML CSS JS Assessment Task/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(show_cupcakes);
+
+var cup_cakes=[
+    {"name":"Chocolate","calories":"high","weight":"200gm"},
+    {"name":"Carrot","calories":"medium","weight":"150gm"},
+    {"name":"Banana","calories":"high","weight":"200gm"},
+    {"name":"Strawberry","calories":"low","weight":"160gm"},
+    {"name":"Peanut","calories":"medium","weight":"200gm"}
+];
+
+
+function show_cupcakes(){
+    //write code that shows the cupcakes in the table as per the instructions
+}
+
+
+
+
+
+function validate(){
+    //write code that validates the form
+    
+}
+
+function show_storage(){
+    //write code that shows the name from local storage
+}
--- OCA_FrontEnd/homeworks/conditionals/conditionals.js
+++ OCA_FrontEnd/homeworks/conditionals/conditionals.js
@@ -1,43 +1,49 @@
+'use strict';
+
 /* 1. Write a JavaScript program that accept two integers and
  * display the larger.Go to the editor */
+
 /******* Start Your Code *********/
-  
-
-
- /******* End Your Code ********* */
+function largerNumber(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
+console.log(largerNumber(1, 2));
+/******* End Your Code ********* */
 
 /* 2. Write a JavaScript conditional statement to find the sign
- of product of three numbers. Display an alert box with the 
- specified sign. Go to the editor 
-Sample numbers : 3, -7, 2 
+ of product of three numbers. Display an alert box with the
+ specified sign. Go to the editor.
+Sample numbers : 3, -7, 2
 Output : The sign is - */
 /******* Start Your Code *********/
-  
+const x = parseInt(document.getElementById('x').value);
+const y = parseInt(document.getElementById('y').value);
+const z = parseInt(document.getElementById('z').value);
 
+const product = Number(x * y * z);
+let sign = product < 0 ? '-' : '+';
+sign = product === 0 ? 'Neither' : sign;
+alert(sign);
+/******* End Your Code ********* */
 
- /******* End Your Code ********* */
-
-/* 3. Write a JavaScript conditional statement to sort three numbers. Display an alert box to show the result. Go to the editor 
-Sample numbers : 0, -1, 4 
+/* 3. Write a JavaScript conditional statement to sort three numbers.
+ Display an alert box to show the result. Go to the editor
+Sample numbers : 0, -1, 4
 Output : 4, 0, -1 */
 /******* Start Your Code *********/
-  
- /******* End Your Code ********* */
 
+/******* End Your Code ********* */
 
-/*4. Write a JavaScript conditional statement to find the largest of five numbers. 
-Display an alert box to show the result. Go to the editor 
-Sample numbers : -5, -2, -6, 0, -1 
+/*4. Write a JavaScript conditional statement to find the largest of five numbers.
+Display an alert box to show the result. Go to the editor
+Sample numbers : -5, -2, -6, 0, -1
 Output : 0 */
 
 /******* Start Your Code *********/
-  
- /******* End Your Code ********* */
 
- /* 5.Write the if statement to display "Hello World" if x is greater than y, otherwise alert "Goodbye".*/
+/******* End Your Code ********* */
+
+/* 5.Write the if statement to display "Hello World" if x is greater than y, otherwise alert "Goodbye".*/
 /******* Start Your Code *********/
-  
- /******* End Your Code ********* */
 
-
-
+/******* End Your Code ********* */
