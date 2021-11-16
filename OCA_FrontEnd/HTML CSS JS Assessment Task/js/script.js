14466f0d50504b21c0583360ccd0e0241873cf1c
ahmadaldabouqii <ahmadaldabouqi97@gmail.com>
2021-11-16 09:27:52 +0200

    FINISH!

--- OCA_FrontEnd/HTML CSS JS Assessment Task/js/script.js
+++ OCA_FrontEnd/HTML CSS JS Assessment Task/js/script.js
@@ -49,7 +49,16 @@ document.querySelector('form').addEventListener('submit', e => {
     allergies: allergies.value,
   };
 
-  if (customer && customer.name !== '' && customer.name.length >= 5 && customer.name.length <= 16 && customer.count !== '' && customer.type !== '' && customer.delivery_time !== '' && customer.delivery_time !== '4:00pm') {
+  if (
+    customer &&
+    customer.name !== '' &&
+    customer.name.length >= 5 &&
+    customer.name.length <= 16 &&
+    customer.count !== '' &&
+    customer.type !== '' &&
+    customer.delivery_time !== '' &&
+    customer.delivery_time !== '4:00pm'
+  ) {
     all_customers.push(customer);
   } else {
     return;
@@ -68,64 +77,96 @@ document.querySelector('form').addEventListener('change', validate);
 
 function validate() {
   if (delivery_time.value === '4:00pm' || delivery_time.value === '') {
-    removeClass(delivery_time,'is-valid');
-    addClass(delivery_time,'is-invalid');
-    setDisplay(delivery_time,'valid-feedback','none');
-    setDisplay(delivery_time,'invalid-feedback','block');
-    delivery_time.closest('.form-group').querySelector('.invalid-feedback').innerHTML = `Unfortunately ${type.value} cannot be delivered at ${delivery_time.value === '' ? 'at this time' : delivery_time.value}`;
+    removeClass(delivery_time, 'is-valid');
+    addClass(delivery_time, 'is-invalid');
+    setDisplay(delivery_time, 'valid-feedback', 'none');
+    setDisplay(delivery_time, 'invalid-feedback', 'block');
+    delivery_time
+      .closest('.form-group')
+      .querySelector('.invalid-feedback').innerHTML = `Unfortunately ${
+      type.value
+    } cannot be delivered at ${
+      delivery_time.value === '' ? 'at this time' : delivery_time.value
+    }`;
   } else {
-    removeClass(delivery_time,'is-invalid');
-    setDisplay(delivery_time,'invalid-feedback','none');
-    setDisplay(delivery_time,'valid-feedback','block');
+    removeClass(delivery_time, 'is-invalid');
+    setDisplay(delivery_time, 'invalid-feedback', 'none');
+    setDisplay(delivery_time, 'valid-feedback', 'block');
   }
 
-  if ((type.value === 'chocolate' && allergies.value === 'dairy_free') || (type.value === 'pecan' && allergies.value === 'nut_free')) {
-    removeClass(type,'is-valid');
-    addClass(type,'is-invalid');
-    setDisplay(type,'valid-feedback','none');
-    setDisplay(type,'invalid-feedback','block');
-    addClass(allergies,'is-invalid');
-    type.closest('.form-group').querySelector('.invalid-feedback').innerHTML = `you can't take  ${type.value} if you have ${allergies.value}`;
+  if (
+    (type.value === 'chocolate' && allergies.value === 'dairy_free') ||
+    (type.value === 'pecan' && allergies.value === 'nut_free')
+  ) {
+    removeClass(type, 'is-valid');
+    addClass(type, 'is-invalid');
+    setDisplay(type, 'valid-feedback', 'none');
+    setDisplay(type, 'invalid-feedback', 'block');
+    addClass(allergies, 'is-invalid');
+    type
+      .closest('.form-group')
+      .querySelector(
+        '.invalid-feedback'
+      ).innerHTML = `you can't take  ${type.value} if you have ${allergies.value}`;
   } else {
-    removeClass(allergies,'is-invalid');
-    removeClass(type,'is-invalid');
-    setDisplay(type,'invalid-feedback','none');
+    removeClass(allergies, 'is-invalid');
+    removeClass(type, 'is-invalid');
+    setDisplay(type, 'invalid-feedback', 'none');
   }
 }
 
-function setDisplay(forWhat, cls, type){
-  forWhat.closest('.form-group').querySelector(`.${cls}`).style.display = `${type}`;
+function setDisplay(forWhat, cls, type) {
+  forWhat
+    .closest('.form-group')
+    .querySelector(`.${cls}`).style.display = `${type}`;
 }
 
-function removeClass(type, clas){
-  type.closest('.form-group').querySelector('.form-select').classList.remove(`${clas}`);
+function removeClass(type, clas) {
+  type
+    .closest('.form-group')
+    .querySelector('.form-select')
+    .classList.remove(`${clas}`);
 }
 
-function addClass(type, clas){
-  type.closest('.form-group').querySelector('.form-select').classList.add(`${clas}`);
+function addClass(type, clas) {
+  type
+    .closest('.form-group')
+    .querySelector('.form-select')
+    .classList.add(`${clas}`);
 }
 
 document.querySelector('.btn-info').addEventListener('click', () => {
   let retrieveUsers = localStorage.getItem('allCustomers');
   if (retrieveUsers) {
     retrieveUsers = JSON.parse(retrieveUsers);
-    document.querySelector('#welcome').innerHTML = `Welcome ${retrieveUsers[retrieveUsers.length - 1].name}`;
+    document.querySelector('#welcome').innerHTML = `Welcome ${
+      retrieveUsers[retrieveUsers.length - 1].name
+    }`;
   }
 });
 
 (function () {
   'use strict';
-  window.addEventListener('load',function () {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function (form) {
-      form.addEventListener('submit',function (event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      },false);
-    });},false);
+  window.addEventListener(
+    'load',
+    function () {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          'submit',
+          function (event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+          },
+          false
+        );
+      });
+    },
+    false
+  );
 })();
